e644783493a3474681262fcb2091344104c1c72b
harsh000777 <harshgoyal7705@gmail.com>
2024-04-24 22:13:16 +0530

    commit1

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'outerBack': '#F1F1F1',
+        'oran': '#FF6F4F',
+        'flee': '#F2ECF7',
+        'fleeb': '#C6C1CA',
+        'fleebox1': '#150D1C',
+        'fleebox': '#432957',
+        'fleebb1': '#29262B',
+        'trii': '#F4E8E4',
+        'triib': '#FFCEBF',
+        'triib1': '#FF8D69',
+        'triib1g': '#CFA1FE',
+        'triibb': '#442925',
+        'triibbtn': '#FF715D',
+        'triib2': '#3E7960',
+        'triibb2': '#1A2A23',
+        'triibbt': '#88918D',
+        'triibbt2': '#80F2E1',
+        'triibbtn2': '#3E7960',
+        'triib3': '#29262B',
+
+
+        
+      },
+    },
+  },
+  plugins: [],
+}
